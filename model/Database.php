46368aa5c95aa930e4c24d51b691fcdbebdaf192
Jacob Tomesh <jacobtomesh2@gmail.com>
2026-01-25 16:11:12 -0600

    Init push

---
+++ model/Database.php
@@ -0,0 +1,22 @@
+<?php
+    class Database{
+        private static $dsn = 'mysql:host=db;dbname=earth';
+        private static $dbUsername = 'mgs_user';
+        private static $dbPassword = 'pa55word';
+        private static $db;
+
+        public static function getDb(){
+            if(!isset(self::$db)){
+                try {
+                    self::$db = new PDO(self::$dsn, self::$dbUsername, self::$dbPassword);
+                    self::$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                } catch (PDOException $e) {
+                    $error_message = $e->getMessage();
+                    include('database_error.php');
+                    exit();
+                }
+            }
+            return self::$db;
+        }
+    }
+?>
